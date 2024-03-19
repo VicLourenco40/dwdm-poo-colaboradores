0e9da6f671ba8ec972917fd919cdbb4b8f951199
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-03-19 16:26:15 +0000

    Add class Colaboradores and constructor method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,28 @@
-﻿namespace DwdmPooColaboradores
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+﻿namespace DwdmPooColaboradores {
+    public class Colaboradores {
+        public const double SubsidioAlimentacao = 140;
+
+        int codigo;
+        string nome;
+        double vencimento;
+        double plafondAlimentacao;
+        bool seguroSaude;
+
+        public Colaboradores(int codigo, string nome, double vencimento,
+                             bool plafondAlimentacao, bool seguroSaude) {
+            this.codigo = codigo;
+            this.nome = nome;
+            this.vencimento = vencimento;
+            this.seguroSaude = seguroSaude;
+            if (plafondAlimentacao) {
+                this.plafondAlimentacao = SubsidioAlimentacao;
+            }
+        }
+    }
+
+    internal class Program {
+        static void Main(string[] args) {
+            Colaboradores[] colaboradores = [];
         }
     }
 }
