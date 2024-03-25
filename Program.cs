61a5584f9a99ebdba658e2eabdd8065baac5b05b
Vicente Lourenço <134103080+VicLourenco40@users.noreply.github.com>
2024-03-25 18:27:53 +0000

    Add menu system and other small changes - Add menu system with example option - Change console encoding to UTF-8 - Explicitly private Colaborador's variables

--- Program.cs
+++ Program.cs
@@ -2,14 +2,14 @@
     public class Colaborador {
         public const double SubsidioAlimentacao = 140;
 
-        int codigo;
-        string nome;
-        double vencimento;
-        double plafondAlimentacao;
-        bool seguroSaude;
+        private int codigo;
+        private string nome;
+        private double vencimento;
+        private double plafondAlimentacao;
+        private bool seguroSaude;
 
         public Colaborador(int codigo, string nome, double vencimento,
-                             bool plafondAlimentacao, bool seguroSaude) {
+                           bool plafondAlimentacao, bool seguroSaude) {
             this.codigo = codigo;
             this.nome = nome;
             this.vencimento = vencimento;
@@ -21,8 +21,47 @@
     }
 
     internal class Program {
+        private static Colaborador[] colaboradores = [];
+
+        public static void Exemplo() {
+            Console.Write($"Número de colaboradores: {colaboradores.Length}\n\n");
+        }
+
+        public static void Menu() {
+            Console.Clear();
+            Console.Write("Gestão de colaboradores\n\n" +
+                          "1. Exemplo\n" +
+                          "0. Sair\n\n" +
+                          ": ");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+
+            switch (opcao) {
+                case 1:
+                    Exemplo();
+                    break;
+
+                case 0:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.Write("Opção Inválida.\n\n");
+                    break;
+            }
+
+            Console.Write("Prima ENTER para continuar...");
+            Console.ReadLine();
+        }
+
         static void Main(string[] args) {
-            Colaborador[] colaboradores = [];
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            while (true) {
+                Menu();
+            }
         }
     }
 }
